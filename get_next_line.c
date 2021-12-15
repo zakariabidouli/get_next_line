67ffbfc4824e46611c7ec24503e836bee81efe00
Zakaria Bidouli <zbidouli@e2r12p1.1337.ma>
2021-12-15 22:59:39 +0100

    get

--- get_next_line.c
+++ get_next_line.c
@@ -1,21 +1,35 @@
 #include "get_next_line.h"
 
+int get_newline(int fd)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while ( c == )
+		if (c == "\n")
+			return(0);
+		i++;
+	}
+	return (i);
+}
+
 char *get_next_line(int fd)
 {
 	static int 	count;
-	char c;
+	int it;
+	int len;
 	char *ptr;
 	
 	count = 0;
 	while (++count)
 	{
-		c = read(fd, sizeof(char), 1);
-		if(c == "\n" || !c)
-			break;
-		ptr = malloc(sizeof(char));
+		len = get_newline(fd);
+		ptr = malloc((sizeof(char)) * (len + 2));
 		if (!ptr)
 			return (NULL);
-		ptr[count] = c; 
+		it = 0;
+		substr()
 	}
 	return (ptr);
 }
