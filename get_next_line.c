6aa176540a82833d958e5c7123e5055b1ec4d151
Zakaria Bidouli <zbidouli@e2r4p12.1337.ma>
2021-12-22 23:20:19 +0100

    get

--- get_next_line.c
+++ get_next_line.c
@@ -5,7 +5,7 @@ int get_newline(char	*c)
 	int i;
 
 	i = 0;
-	while (c[i] != '\n' || !c[i])
+	while (c[i] != '\n' && c[i])
 		i++;
 	return (i);
 }
@@ -13,39 +13,58 @@ int get_newline(char	*c)
 char *get_next_line(int fd)
 {
 	size_t			len;
-	size_t			len_total;
-	char			*ptr;
-	static char			*p;
+	char			*buffer;
+	static char		*line;
 
-	len_total = 0;
-	p = ft_strdup("");
-	if (!p)
-			return (NULL);
-	len_total = read (fd, p, sizeof (fd));
-	ptr = NULL;
-	len = 0;
-	while (++ptr)
+	line = ft_strdup("");
+	buffer = ft_strdup("");
+	if (!line)
+		return NULL;
+	while (get_newline(buffer))
 	{
-		ptr = malloc((sizeof(char)) * (BUFFER_SIZE + 1));
-		if (!ptr)
-			return (NULL);
-		len = read (fd, ptr, (BUFFER_SIZE));
-
-		if (len != BUFFER_SIZE)
-			return (0); //error
-		if (len != (size_t) get_newline(ptr))
+		buffer = malloc(sizeof(char) * BUFFER_SIZE);
+		if (!buffer)
+			return NULL; 
+		len = read (fd, buffer, BUFFER_SIZE);
+		if (len <= (size_t) get_newline(buffer))
 		{	
-			ptr = ft_substr(ptr, 0, get_newline(ptr));
-			ptr[get_newline(ptr) + 1] = '\0';
-			break ;
+			buffer = ft_substr(buffer, 0, get_newline(buffer));
+			buffer[get_newline(buffer) + 1] = '\0';
 		}
+		line = ft_strjoin(line, buffer);
+		line [ft_strlen(line) + 1] = '\n'; 
+		line [ft_strlen(line) + 2] = '\0'; 
 	}
-	p = ft_strjoin(p, ptr); 
-	return (ptr);
+	return (line);
 }
 
 int main ()
 {
 	int fd = open ("txt.txt", O_RDONLY);
-	printf("%s",get_next_line(fd));
+	printf("[%s]",get_next_line(fd));
 }
+
+
+	// len_total = 0;
+	// p = ft_strdup("");
+	// if (!p)
+	// 		return (NULL);
+	// len_total = read (fd, p, sizeof (fd));
+	// ptr = NULL;
+	// len = 0;
+	// while (++ptr)
+	// {
+	// 	ptr = malloc(sizeof(char) * BUFFER_SIZ);
+	// 	if (!ptr)
+	// 		return (NULL);
+	// 	len = read (fd, ptr, (BUFFER_SIZE));
+
+	// 	if (len != BUFFER_SIZE)
+	// 		return (0); //error
+	// 	if (len  (size_t) get_newline(ptr))
+	// 	{	
+	// 		ptr = ft_substr(ptr, 0, get_newline(ptr));
+	// 		ptr[get_newline(ptr) + 1] = '\0';
+	// 		break ;
+	// 	}
+	// }
